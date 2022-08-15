6fa0720d43bfd29405296f9d168459ad74cc7368
Toby Comon <toby.comon@gmail.com>
2022-08-15 13:31:48 -0600

    Initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,79 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+      <div class="d-flex flex-column align-items-center">
+        <!-- <img alt="logo" src="../assets/img/cw-logo.png" height="45" /> -->
+        <h2>JAMES MUSCHLER</h2>
+      </div>
+    </router-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarText"
+      aria-controls="navbarText"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto">
+        <li>
+          <div
+            v-scroll-to="'bio'"
+            class="btn text-success lighten-30 selectable text-uppercase"
+          >
+            BIO
+          </div>
+        </li>
+      </ul>
+      <!-- LOGIN COMPONENT HERE -->
+      <Login />
+    </div>
+  </nav>
+</template>
+
+<script>
+var Vue = require("vue");
+var VueScrollTo = require("vue-scrollto");
+
+Vue.use(VueScrollTo);
+
+// You can also pass in the default options
+Vue.use(VueScrollTo, {
+  container: "body",
+  duration: 500,
+  easing: "ease",
+  offset: 0,
+  force: true,
+  cancelable: true,
+  onStart: false,
+  onDone: false,
+  onCancel: false,
+  x: false,
+  y: true,
+});
+export default {
+  directives: {
+    VueScrollTo,
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+a:hover {
+  text-decoration: none;
+}
+.nav-link {
+  text-transform: uppercase;
+}
+.navbar-nav .router-link-exact-active {
+  border-bottom: 2px solid var(--bs-success);
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,79 @@
+<template>
+  <div
+    class="
+      home
+      flex-grow-1
+      d-flex
+      flex-column
+      align-items-center
+      justify-content-center
+    "
+  >
+    <div class="home-card p-5 bg-white rounded elevation-3 mb-5">
+      <img
+        src="https://bcw.blob.core.windows.net/public/img/8600856373152463"
+        alt="CodeWorks Logo"
+        class="rounded-circle"
+      />
+      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
+        Vue 3 Starter
+      </h1>
+    </div>
+  </div>
+  <div class="container my-5">
+    <div class="row">
+      <div class="col">
+        <img src="https://thiscatdoesnotexist.com" alt="" />
+      </div>
+    </div>
+  </div>
+  <div id="bio" class="mt-5 bio container-fluid d-flex flex-grow-1">
+    <div class="row">
+      <div class="col">
+        BIO Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore,
+        recusandae! Nulla id cumque ut quibusdam recusandae architecto neque
+        odio quisquam? Suscipit veniam voluptate alias esse vitae non modi amet
+        qui? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        temporibus sunt earum, ullam tenetur nemo atque eligendi perferendis,
+        rem porro animi adipisci dolore quo hic laudantium sint cum magni sequi.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed dicta
+        ullam impedit neque in cum explicabo, cumque sunt expedita laudantium
+        tempore, amet ea unde, rem blanditiis dolor? Incidunt, animi quis?
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "Home",
+
+  setup() {
+    return {
+      bio: ref.bio,
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.home {
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+  .home-card {
+    width: 50vw;
+    > img {
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
+}
+</style>
